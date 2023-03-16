25a208ba98f5137650cad05320a9241a9c1e1648
rachelqian777 <jiaqiqia@usc.edu>
2023-03-16 10:35:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
               Learn <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
+              Rachel's Personal Site
             </p>
           </a>
 
